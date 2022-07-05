256d6690fc1de4f37720df69b5c36c1a49c7f566
Ching-wen Hu <ching-wen.hu@developersinstitute.co.nz>
2022-07-05 22:09:36 +1200

    fix gap in css by adding reset setting & test out basic display in js

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-const formulaDispaly = document.querySelector(".formulaDisplay")
+const formulaDisplay = document.querySelector(".formulaDisplay");
 const mainDisplay = document.querySelector(".mainDisplay")
+
+const numberGrp = document.querySelector(".NumButton_group");
+
+const allClear = document.querySelector(".allClear")
+
+const deleteBtn = document.querySelector(".delete")
+
+const equal = document.querySelector(".equal")
+
+// if all clear button is clicked, the main display & forumulaDisplay will both return to 0
+allClear.addEventListener('click',function(){
+  mainDisplay.textContent= 0
+  formulaDisplay.textContent = 0;
+})
+
+numberGrp.addEventListener('click', function(e){
+   if(e.target.nodeName ==="LI"){
+    console.log(e.target)
+    let key = e.target.textContent;
+    console.log(key)
+    mainDisplay.textContent = key;
+  }
+})
+
+
+
+
 
